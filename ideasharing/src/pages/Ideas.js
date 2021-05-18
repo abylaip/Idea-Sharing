292c554502936432678a89e85490a99120852bd5
zhandos <>
2021-05-18 09:44:32 +0600

    undone design version

--- ideasharing/src/pages/Ideas.js
+++ ideasharing/src/pages/Ideas.js
@@ -1,9 +1,63 @@
 import React from "react";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
+import "../assets/css/Ideas.css";
 
 export default function Ideas() {
   return (
-    <div>
-      <h1>IDEAS</h1>
+    <div className="ideas-body">
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Search..."
+          aria-label="Search"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+      <div className="ideas-idea">
+        <h4>Zhandos Abdireshov</h4>
+        <p>13.05.2021</p>
+        <h4>Social Media</h4>
+        <p>
+          Social media are interactive technologies that allow the creation or
+          sharing/exchange of information, ideas, career interests, and other
+          forms of expression via virtual communities and networks.[1][2] While
+          challenges to the definition of social media arise[3] due to the broad
+          variety of stand-alone and built-in social-media services currently
+          available, there are some common features:[2]
+        </p>
+        <Button variant="dark">Connect</Button>
+        <Button variant="link">More info</Button>
+      </div>
+      <div
+        style={{
+          borderTop: "1px solid gray",
+          marginTop: 5,
+          marginBottom: 5,
+          marginLeft: 10,
+          marginRight: 10,
+        }}
+      ></div>
+      <div className="ideas-idea">
+        <h4>Abylay Aiyp</h4>
+        <p>16.05.2021</p>
+        <h4>Smart smartphone</h4>
+        <p>
+          A smartphone is a portable device that combines mobile telephone and
+          computing functions into one unit. They are distinguished from feature
+          phones by their stronger hardware capabilities and extensive mobile
+          operating systems, which facilitate wider software, internet
+          (including web browsing over mobile broadband), and multimedia
+          functionality (including music, video, cameras, and gaming), alongside
+          core phone functions such as voice calls and text messaging.
+          Smartphones typically contain a number of metal–oxide–semiconductor
+          (MOS) integrated circuit (IC) chips, include various sensors that can
+          be leveraged by pre-included and third-party software (such as a
+          magnetometer, proximity sensors, barometer, gyroscope, accelerometer
+          and more), and support wireless communications protocols (such as
+          Bluetooth, Wi-Fi, or satellite navigation).
+        </p>
+        <Button variant="dark">Connect</Button>
+        <Button variant="link">More info</Button>
+      </div>
     </div>
   );
 }
--- ideasharing/src/pages/Profile.js
+++ ideasharing/src/pages/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Ava from "../assets/ava.jpeg";
 import "../assets/css/Profile.css";
-import { InputGroup, FormControl } from "react-bootstrap";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
 
 export default function Profile() {
   return (
@@ -26,28 +26,93 @@ export default function Profile() {
             <p>Skills: </p>
             <p>Full-stack developer</p>
           </div>
+          <div className="personal-info-each">
+            <p>Projects: </p>
+            <p>IS.kz</p>
+          </div>
         </div>
       </div>
-      <br />
-      <br />
-      <div className="profile-ideas">
+      <div
+        style={{
+          borderTop: "1px solid gray",
+          marginLeft: 10,
+          marginRight: 10,
+        }}
+      ></div>
+      <div className="profile-create-ideas">
         <InputGroup className="mb-3">
-          <InputGroup.Prepend>
-            <InputGroup.Text id="inputGroup-sizing-default">
-              Default
-            </InputGroup.Text>
-          </InputGroup.Prepend>
           <FormControl
-            aria-label="Default"
+            aria-label="Title"
             aria-describedby="inputGroup-sizing-default"
+            placeholder="Title"
           />
         </InputGroup>
-        <InputGroup>
+        <InputGroup size="sm">
           <InputGroup.Prepend>
             <InputGroup.Text>Description</InputGroup.Text>
           </InputGroup.Prepend>
           <FormControl as="textarea" aria-label="Description" />
         </InputGroup>
+        <br />
+        <Button variant="success" size="lg" block>
+          Submit idea
+        </Button>
+      </div>
+      <div
+        style={{
+          borderTop: "1px solid gray",
+          marginTop: 5,
+          marginBottom: 5,
+          marginLeft: 10,
+          marginRight: 10,
+        }}
+      ></div>
+      <div className="profile-ideas">
+        <div className="profile-idea">
+          <h4>Abylay Aiyp</h4>
+          <p>14.05.2021</p>
+          <h4>GPS System</h4>
+          <p>
+            The Global Positioning System (GPS), originally Navstar GPS,[1] is a
+            satellite-based radionavigation system owned by the United States
+            government and operated by the United States Space Force.[2] It is
+            one of the global navigation satellite systems (GNSS) that provides
+            geolocation and time information to a GPS receiver anywhere on or
+            near the Earth where there is an unobstructed line of sight to four
+            or more GPS satellites.[3] Obstacles such as mountains and buildings
+            block the relatively weak GPS signals.
+          </p>
+        </div>
+        <div
+          style={{
+            borderTop: "1px solid gray",
+            marginTop: 5,
+            marginBottom: 5,
+            marginLeft: 10,
+            marginRight: 10,
+          }}
+        ></div>
+        <div className="profile-idea">
+          <h4>Abylay Aiyp</h4>
+          <p>16.05.2021</p>
+          <h4>Smart smartphone</h4>
+          <p>
+            A smartphone is a portable device that combines mobile telephone and
+            computing functions into one unit. They are distinguished from
+            feature phones by their stronger hardware capabilities and extensive
+            mobile operating systems, which facilitate wider software, internet
+            (including web browsing over mobile broadband), and multimedia
+            functionality (including music, video, cameras, and gaming),
+            alongside core phone functions such as voice calls and text
+            messaging. Smartphones typically contain a number of
+            metal–oxide–semiconductor (MOS) integrated circuit (IC) chips,
+            include various sensors that can be leveraged by pre-included and
+            third-party software (such as a magnetometer, proximity sensors,
+            barometer, gyroscope, accelerometer and more), and support wireless
+            communications protocols (such as Bluetooth, Wi-Fi, or satellite
+            navigation).
+          </p>
+        </div>
       </div>
     </div>
   );
